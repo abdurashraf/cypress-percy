166eaf8482b01396b1699da62a509c48a8549a3c
Abdur.Ashraf <abdur.ashraf@rabobank.nl>
2023-04-07 11:32:03 +0200

    first commit

---
+++ cypress/e2e/specs/ first-test.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+describe('Cypress Visual Testing', () => {
+    it('Compare landing page of SwagLabs', () => {
+        cy.visit("http://localhost:3000/");
+        cy.get('[data-test="username"]').type("standard_user");
+        cy.get('[data-test="password"]').type("secret_sauce");
+        cy.get('[data-test="login-button"]').click()
+        cy.percySnapshot("Inventory");
+    })
+})
